950cfa97ddabfd0b2ac987f06409396f55e76070
Lucas Santos <55330008+lukkinhas95@users.noreply.github.com>
2025-09-09 01:14:36 +0000

    20250908-push2

--- 20250908.py
+++ 20250908.py
@@ -2,40 +2,74 @@
 #Pergunta: Com python: peça ao usuário que digite seu nome e imprima uma saudação simples.
 #nome = input("Digite seu nome: ")
 #print (nome)
-
+#
 #Pergunta: Solicite ao usuário que digite sua idade e exiba a mensagem "Você tem X anos".
 #idade = input("Digite a sua idade: ")
 #idade_calculo = int(idade)
 #print(f"Daqui a cinco anos você terá {idade_calculo + 5}")
-
+#
 #Pergunta: Pergunte ao usuário qual é a sua cor favorita e mostre a resposta.
 #cor = input("Digite sua cor favorita: ")
 #print(cor)
-
+#
 #Nível 2
 #Pergunta: Peça ao usuário dois números e armazene-os em variáveis. Em seguida, exiba a soma.
-
+#
 #n1 = float(input("Digite o primeiro número: \n"))
 #n2 = float(input("Digite o segundo número: "))
 #input (n1+n2)
-
+#
 #Pergunta: Solicite o nome e a idade do usuário, armazene em variáveis e mostre: "Nome: [nome], Idade: [idade]".
 #nome = input("Digite seu nome: ")
 #idade = int(input("Digite sua idade"))
 #print(f"Nome: [{nome}], Idade: [{idade}]")
-
+#
 #Pergunta: Peça ao usuário seu nome, idade e cidade. Armazene essas informações em um dicionário e exiba-o.
+#
+#nome = input(" Digite o seu nome: ")
+#idade = float(input("Digite a sua idade: "))
+#cidade = input("Qual cidade você nasceu: ")
+#dic = {
+#    "O nome do cabra é": nome,
+#    "A idade do cabra é": idade,
+#    "O cabra nasceu em": cidade
+#}
+#
+#lista = []
+#lista.append(dic)
+#
+#print(lista)
+#
+#Pergunta: Solicite o nome de um produto, seu preço e quantidade em estoque. Crie um dicionário com essas informações e imprima.
+#
+#prod_1 = input("Qual o produto desejado: ")
+#preco_1 = float(input("Qual o preço do produto: "))
+#quant_1 = float (input("Quantos produtos em estoque: "))
+#prod_2 = input("Qual o produto desejado 2: ")
+#preco_2 = float(input("Qual o preço do produto 2: "))
+#quant_2 = float (input("Quantos produtos em estoque 2: "))
+#
+#dic_prod1 = {
+#    "O nome do produto é" :prod_1,
+#    "O preço do produto é" :preco_1,
+#    "O Produto está com a quatidade de": quant_1
+#}
+#
+#dic_prod2 = {
+#    "O nome do produto é o produto 2" :prod_2,
+#    "O preço do produto 2 é" :preco_2,
+#    "O Produto 2 está com a quatidade de": quant_2
+#}
+#
+#
+#lista_1 = []
+#lista_1.append(dic_prod1)
+#print(lista_1)
+#
+#lista_2 = []
+#lista_2.append(dic_prod2)
+#print(lista_2)
 
-nome = input(" Digite o seu nome: ")
-idade = float(input("Digite a sua idade: "))
-cidade = input("Qual cidade você nasceu: ")
-dic = {
-    "O nome do cabra é": nome,
-    "A idade do cabra é": idade,
-    "O cabra nasceu em": cidade
-}
+#Pergunta: Crie um programa que coleta dados de 2 pessoas (nome e idade) e armazena cada pessoa como um dicionário em uma lista. Mostre a lista ao final.
 
-lista = []
-lista.append(dic)
 
-print(lista)
