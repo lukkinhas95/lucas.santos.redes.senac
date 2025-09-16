b414ef8297c79491b42bc3b919c76eb52bdd88f9
Lucas Santos <55330008+lukkinhas95@users.noreply.github.com>
2025-09-16 00:27:00 +0000

    dia 15

---
+++ 20250915 - exercicios.py
@@ -0,0 +1,53 @@
+#Programa 
+
+#A = 1
+#B = 0
+#if A == 1 or B == 1:
+#    print("Saída: 1")
+#else:
+#    print("Saída: 0")
+
+
+#A = 1
+#B = 2
+#if A == 1 and B == 1:
+#    print("Saída: 1")
+#else:
+#    print("Saída: 0")
+
+input("qual a velocidade?")
+vel=(input)
+
+if vel >= 40:
+    print("Parabens, você não foi multado!")
+else:    
+    print("Acima de 40, multado e taxado!!!!!!!")
+
+
+    #A = 0
+#if A == 1:
+#    print("Saída: 0")
+#else:
+#    print("Saída: 1")
+
+
+
+
+
+
+
+cara = print = ("escolha cara (1) ou coroa (0):")
+
+if cara == 0:
+    print("Parabéns, você perdeu!")
+else:
+    print("Perdeu muito otário!")
+
+
+#A = 1
+#B = 0
+#if A != B:
+#    print("Saída: 1")
+#else:
+#    print("Saída: 0")
+
---
+++ 20250915.py
@@ -0,0 +1,9 @@
+#Estudo de caso Sistema de Controle de Alarme com Portas Lógicas
+
+fum = "" 
+bt_emg = ""
+ch_sec = ""
+mov = ""
+
+if fum = 1 : 
+else
